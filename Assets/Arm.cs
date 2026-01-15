76762239d0a7f3a6aeabc615215c391fd35f08e3
Kimjunsoo1032 <k024g1032@g.neec.ac.jp>
2026-01-15 10:27:59 +0900

    02_03

--- Assets/Arm.cs
+++ Assets/Arm.cs
@@ -1,40 +1,60 @@
+﻿using NUnit.Framework;
 using UnityEngine;
 
 public class Arm : MonoBehaviour
 {
     private GunBase gun_;
+    private Cursor cursor_;
+
     public void Grab(GunBase gun)
     {
-        if (gun_ != null) { Destroy(gun_.gameObject); }
+        if (gun_ != null)
+        {
+            Destroy(gun_.gameObject);
+        }
+
         gun_ = gun;
         gun.transform.SetParent(transform);
         gun.transform.localPosition = Vector3.zero;
-        gun.transform.localRotation =
-            Quaternion.identity;
+        gun.transform.localRotation = Quaternion.identity;
     }
+
     public bool IsGrabGun()
     {
         return gun_ != null;
     }
+
     public void OnTrigger()
     {
-        if (!IsGrabGun()) { return; }
+        if (!IsGrabGun()) return;
         gun_.OnTrigger();
     }
+
     public void OffTrigger()
     {
-        if (!IsGrabGun()) { return; }
+        if (!IsGrabGun()) return;
         gun_.OffTrigger();
     }
-// Start is called once before the first execution of Update after the MonoBehaviour is created
-void Start()
-{
 
-}
+    void Start()
+    {
+        GameObject cameraObject =
+            GameObject.FindGameObjectWithTag("MainCamera");
+        Assert.IsNotNull(cameraObject);
 
-// Update is called once per frame
-void Update()
-{
+        bool isFindCursor =
+            cameraObject.TryGetComponent(out cursor_);
+        Assert.IsTrue(isFindCursor);
+    }
+    private void Update()
+    {
+        Vector3 cursorPoint =
+            cursor_.GetRaycastHit().point;
 
-}
+        cursorPoint.y =
+            transform.position.y - cursorPoint.y;
+
+        transform.LookAt(cursorPoint);
+    }
+
 }
